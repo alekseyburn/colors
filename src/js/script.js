0a82794bebc7bc8c7407e2564cbfad443571252d
alekseyburn <alekseyburn@gmail.com>
2020-05-14 17:21:52 +0300

    функция генерации цвета, добавление цвета к div, функция контраста текста, функция раскраски слайдеров.

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,54 @@
 /* eslint-disable no-undef */
-// const ready = require('./utils/documentReady.js');
+const colorDivs = document.querySelectorAll('.color');
+// const generateBtn = document.querySelector('.generate');
+// const sliders = document.querySelectorAll('input[type="range"]');
+// const currentHexes = document.querySelectorAll('.color h2');
+// let initialColors;
 
-// ready(function(){
-// });
-
-// const $ = require('jquery');
-// $( document ).ready(function() {});
-
-document.querySelector('html').classList.remove('no-js');
-if (/Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor)) {
-  document.querySelector('html').classList.remove('webp');
-  document.querySelector('html').classList.add('no-webp');
+//Color generator
+function generateHex() {
+  return chroma.random();
 }
+
+function randomColors() {
+  colorDivs.forEach((div) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    //Add the color to the bg
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+    //Check for contrast
+    checkTextContrast(randomColor, hexText);
+    //Initial colorize sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll('.sliders input');
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
+  })
+}
+
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  luminance > 0.5 ? text.style.color = 'black' : text.style.color = 'white';
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  //Scale Saturation
+  const noSat = color.set('hsl.s', 0);
+  const fullSat = color.set('hsl.s', 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+  //Scale Brightness
+  const midBright = color.set('hsl.l', 0.5);
+  const scaleBright = chroma.scale(['black', midBright, 'white']);
+
+  //Update input colors
+  saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(1)}`;
+  brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(0)}, ${scaleBright(0.5)}, ${scaleBright(1)}`;
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75), rgb(204,204,75), rgb(75,204,75), rgb(75,204,204), rgb(75,75,204), rgb(204,75,204), rgb(204,75,75))`
+}
+randomColors();
